000a5dc2a08fef316a4856c2db70513a213760df
DevLomoSE <jonathancggs@gmail.com>
2021-10-17 16:34:47 -0500

    :tada: Init Project

---
+++ src/test/java/com/devlomose/app/ms/eurekaserver/SbEurekaServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlomose.app.ms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEurekaServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
